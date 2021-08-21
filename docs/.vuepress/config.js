0a7919b18d2139aa5d739d810f9a28383e119ea0
duoli <zhaody901@126.com>
2021-08-21 19:07:55 +0800

    Logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,9 @@ const sidebar = require("./sidebar.js");
 const navbar = require("./navbar.js");
 module.exports = {
 	lang: "zh-CN",
-	title: "你好， VuePress ！",
-	description: "这是我的第一个 VuePress 站点",
+	title: "学习笔记@小有记",
 	themeConfig: {
-		logo: "https://vuejs.org/images/logo.png",
+		logo: "/images/logo.png",
 		sidebar,
 		navbar,
 		displayAllHeaders: true,
